c3da62f5234c35e71f554d097e0aacf0ea33680c
Antoine <antoine.morin.paulhus@gmail.com>
2017-11-13 22:27:07 -0500

    improve user templates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.action_mailer.delivery_method = :sendmail
+  
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
