ee34db5528733361bfc36d706011511ccabcc33b
Antoine <antoine.morin.paulhus@gmail.com>
2017-11-13 21:37:40 -0500

    Base user setup

---
+++ app/controllers/gif_controller.rb
@@ -0,0 +1,2 @@
+class GifController < ApplicationController
+end
---
+++ test/controllers/gif_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GifControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
