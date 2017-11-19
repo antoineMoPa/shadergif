ebfc4e6f1035e4bd14ae70ab0ad8701ba62faa8f
Antoine <antoine.morin.paulhus@gmail.com>
2017-11-19 13:47:26 -0500

    move shader editor css to an asset

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
+Rails.application.config.assets.precompile += %w( shader_editor.scss )
 Rails.application.config.assets.precompile += %w( components.js )
