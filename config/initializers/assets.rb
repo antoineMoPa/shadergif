5821d49ccdd83f489f1ca883bc0085b42f4d6c21
Antoine <antoine.morin.paulhus@gmail.com>
2017-11-20 19:38:17 -0500

    Reorganize shader editor assets to integrate rails

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,3 +15,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile += %w( shader_editor.scss )
 Rails.application.config.assets.precompile += %w( components.js )
+Rails.application.config.assets.precompile += %w( shader_editor.js )
