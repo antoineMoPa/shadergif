19d571c191eea977f250e20bbcb8f1f5fc3b8a33
Antoine <antoine.morin.paulhus@gmail.com>
2017-11-22 00:24:22 -0500

    work on emails, change noreply address

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :sendmail
   
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
