f2ffce0bb279f46e5d1c37fd3964d9108857c76c
Antoine <antoine.morin.paulhus@gmail.com>
2017-11-26 19:34:49 -0500

    Find better preview / animation compromise to reduce bandwidth usage

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -8,11 +8,14 @@ class Gif < ApplicationRecord
     # Extract all frames
     `timeout 10 convert public/gifs/#{filename} public/gifs/generated/#{filename}-temp-%04d.png`
     # Create gif video
-    `avconv -y -r 10 -i public/gifs/generated/#{filename}-temp-%04d.png -crf 37 public/gifs/generated/#{filename}-vid.mp4`
-    `avconv -y -r 10 -i public/gifs/generated/#{filename}-temp-%04d.png -crf 37 public/gifs/generated/#{filename}-vid.ogv`
+    `avconv -y -r 10 -i public/gifs/generated/#{filename}-temp-%04d.png -qscale:v 10 public/gifs/generated/#{filename}-vid.mp4`
+    `avconv -y -r 10 -i public/gifs/generated/#{filename}-temp-%04d.png -qscale:v 7 public/gifs/generated/#{filename}-vid.ogv`
     
     # Save first frame as preview
     `mv public/gifs/generated/#{filename}-temp-0000.png public/gifs/generated/#{filename}-preview.png`
+    # Create jpeg preview (smaller, uglier)
+    `convert public/gifs/generated/#{filename}-preview.png -quality 50 public/gifs/generated/#{filename}-preview.jpg`
+    
     # remove generated pngs
     `rm public/gifs/generated/#{filename}-temp-*.png`
   end
