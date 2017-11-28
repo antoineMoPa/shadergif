68b5001e52e8e4a0bb4974dc2d826d0a92d1c2bf
Antoine <antoine.morin.paulhus@gmail.com>
2017-11-27 22:10:27 -0500

    try more video formats

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -6,13 +6,15 @@ class Gif < ApplicationRecord
     # Generate thumbnail
     `timeout 10 convert public/gifs/#{filename} +dither -colors 8 -resize 45x45 public/gifs/generated/#{filename}-small.gif`
     # Extract all frames
-    `timeout 10 convert public/gifs/#{filename} public/gifs/generated/#{filename}-temp-%04d.png`
+    # -scene 1: start at 1 (better for avconv)
+    `timeout 10 convert public/gifs/#{filename} -scene 1 public/gifs/generated/#{filename}-temp-%04d.png`
     # Create gif video
-    `avconv -y -r 10 -i public/gifs/generated/#{filename}-temp-%04d.png -qscale:v 10 public/gifs/generated/#{filename}-vid.mp4`
-    `avconv -y -r 10 -i public/gifs/generated/#{filename}-temp-%04d.png -qscale:v 7 public/gifs/generated/#{filename}-vid.ogv`
+    `avconv -y -r 10 -movflags faststart -i public/gifs/generated/#{filename}-temp-%04d.png -qscale:v 10 -r 10 public/gifs/generated/#{filename}-vid.mp4`
+    `avconv -y -r 10 -i public/gifs/generated/#{filename}-temp-%04d.png -qscale:v 7 -r 10 public/gifs/generated/#{filename}-vid.ogv`
+    `avconv -y -r 10 -i public/gifs/generated/#{filename}-temp-%04d.png  -c:v libvpx -qscale:v 7 -r 10 public/gifs/generated/#{filename}-vid.webm`
     
     # Save first frame as preview
-    `mv public/gifs/generated/#{filename}-temp-0000.png public/gifs/generated/#{filename}-preview.png`
+    `mv public/gifs/generated/#{filename}-temp-0001.png public/gifs/generated/#{filename}-preview.png`
     # Create jpeg preview (smaller, uglier)
     `convert public/gifs/generated/#{filename}-preview.png -quality 50 public/gifs/generated/#{filename}-preview.jpg`
     
---
+++ doc/docs/manual/tips-and-snippets.md
@@ -0,0 +1,18 @@
+# Angles
+
+Find angle of point relative to (0,0):
+
+    float angle = atan(pos.y, pos.x);
+
+# Useful defines
+
+**Put these at the top of your file.**
+
+Define `PI` for future use:
+
+	#define PI 3.14159265359
+
+And why not `PI2`:
+
+	#define PI2 6.28318530718
+
