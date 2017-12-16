61b0d1b85208503018a0690442b3d95d603334de
Antoine <antoine.morin.paulhus@gmail.com>
2017-12-16 00:43:34 -0500

    Use empty compressor...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,13 +22,21 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
+  
+  # https://stackoverflow.com/questions/9674714/disable-asset-minification-in-rails-production
+  class NoCompression
+    def compress(string)
+      # do nothing
+      string
+    end
+  end
+  
   # Compress JavaScripts and CSS.
-  # config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = NoCompression.new
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
