161a6391d5504089253f3ba652d577f1f989a946
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2018-05-21 15:02:54 -0400

    Better handling of generated assets

--- electron/build.bash
+++ electron/build.bash
@@ -30,9 +30,6 @@ for element in "public/assets" "public/icons" "public/images" "public/workers" "
 	cp -r $element $electron_path;
 done
 
-# Cleanup
-rm -r public/assets
-
 cd electron
 
 # editor.js expects this to be there
