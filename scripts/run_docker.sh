6aa14e8b06b4ba11e5467999ae1f24cc020e4f44
Antoine <antoine.morin.paulhus@gmail.com>
2018-10-24 21:45:34 -0400

    Fake gifs when starting docker

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -4,6 +4,8 @@ service mysql start
 
 rails db:create
 rails db:migrate
+echo "Generating fake gifs..."
+rails db:seed
 
 echo ""
 echo "WARNING: DEV VERSION - All saved data (including gifs) is lost at each run"
