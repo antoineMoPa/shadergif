495f043dbb8f5a484253c41a954e101dab94c034
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2018-11-20 15:45:55 -0500

    Remove unused stuff from base.js

--- app/assets/javascripts/lib/base.js
+++ app/assets/javascripts/lib/base.js
@@ -10,36 +10,6 @@ function qsa(sel){
 }
 
 /*
-  qsa, but on an element
- */
-function subqsa(el,sel){
-    return el.querySelectorAll(sel);
-}
-
-/*
-  Hide an element
-  (you need to hide elements with a .hidden css 
-   rule in your css code)
-*/
-function hide(el){
-    el.classList.add("hidden");
-}
-
-/*
-   Determine if hidden
- */
-function hidden(el){
-    return el.classList.contains("hidden");
-}
-
-/*
-  Show an element
- */
-function show(el){
-    el.classList.remove("hidden");
-}
-
-/*
   Load a script
   Returns the content
  */
@@ -47,13 +17,3 @@ function load_script(name){
     var content = qsa("script[name="+name+"]")[0].innerHTML;
     return content;
 }
-
-/*
-  Create a dom element
- */
-function dom(html){
-    var node = document.createElement("div");
-    node.innerHTML = html;
-    return node.children[0];
-}
-
